99f7fd5248b813d20cc79fd4c8adf78f02c4f1ab
Michael Wan <michaelw99@berkeley.edu>
2016-10-05 00:26:19 -0700

    type sorting working

--- main.py
+++ main.py
@@ -1,44 +1,80 @@
 from subprocess import call, check_output
 from os.path import expanduser
 import os
+import shutil
 
 EXIF_INFO = {}
+main_path = ''
 
 def main():
-	print("Welcome to exifSort! \nTo begin, input your image folder: ")
-	path = raw_input()
+	print("Welcome to exifSort! \nTo begin, input your image folder path: ")
+	path = raw_input('> ')
 	import_data(path)
 
 	print("Import complete. You may now input commands. Enter 'help' for a list of commands.")
 	while True:
-		command = raw_input('>')
+		command = raw_input('> ')
+		if command == 'swap':
+			new_path = raw_input('Please enter the new folder path to sort: ')
+			swap(new_path)
+		elif command == 'type':
+			copy = raw_input('Keep copy of original files? (Y/N): ')
+			if copy == 'Y':
+				x = True
+				type(x)
+			elif copy == 'N':
+				x = False
+				type(x)
+			else:
+				print('Please only enter Y or N.')
+		elif command == 'help':
+			print("You're outta luck bub.")
+		else:
+			print("Please input a proper command.")
 
-		pass
-
+# gets all exif data from folder
 def import_data(path):
 	# weird input? Clarify. Right now, no home directory or beginning / needed.
-	edit_path = expanduser("~") + '/' + path
-	for filename in os.listdir(edit_path):
-		exif = check_output(['exiv2', '-p', 's', edit_path + '/' + filename])
-		one = exif.split('\n')
-		# this may not be safe. Not sure if it's always 4 at the end. CHECK.
-		two = one[:len(one) - 4]
-		dic = {}
-		for item in two:
-			k, v = item.split(':', 1)
-			dic[k.strip()] = v.strip()
-		EXIF_INFO[filename] = dic
-	# print(EXIF_INFO)
+	global main_path 
+	main_path = expanduser("~") + '/' + path
+	for file_name in os.listdir(main_path):
+		if os.path.isfile(main_path + '/' + file_name):
+			exif = check_output(['exiv2', '-p', 's', main_path + '/' + file_name])
+			one = exif.split('\n')
+			# this may not be safe. Not sure if it's always 4 at the end. CHECK.
+			two = one[:len(one) - 4]
+			dic = {}
+			for item in two:
+				k, v = item.split(':', 1)
+				dic[k.strip()] = v.strip()
+			EXIF_INFO[file_name] = dic
 
-# THIS IS THE MOTHERLOAD
-def split_files():
+# moves file to folder, creates folder if necessary
+# maybe add feature to create folder in new destination. Currently creates in place.
+def move_file(file_name, dest_name, copy):
+	if not os.path.exists(dest_name):
+		os.makedirs(dest_name)
+	if copy:
+		shutil.copy2(main_path + '/' + file_name, dest_name + '/' + file_name)
+	else:
+		shutil.move(main_path + '/' + file_name, dest_name + '/' + file_name)
+
+# changes the folder that the program is sorting
+def swap(path):
+	global main_path
+	main_path = expanduser("~") + '/' + path
+
+# organize by image type, maybe allow more granularity (FINE, MEDIUM, etc.)
+def type(copy):
+	for image in EXIF_INFO:
+		if EXIF_INFO[image]['Image quality'] == 'RAW':
+			move_file(image, main_path + '/RAW', copy)
+		else:
+			move_file(image, main_path + '/JPEG', copy)
+
+def shutter(boundaries, copy):
 	pass
 
-def type():
-	pass
-
-def shutter():
-	pass
 
 def iso():
 	pass
