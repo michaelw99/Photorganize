de5f525e66336f2ae919325e8c08ad71388d553b
Michael Wan <michaelw99@berkeley.edu>
2016-10-06 17:51:32 -0700

    auto sort functionality

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from subprocess import call, check_output
-from os.path import expanduser
 import os
 import shutil
 import imghdr
+import time
 
 # priority queue to keep sorted by time? 
 # also add auto sorting
@@ -32,55 +32,100 @@ def main():
 				print('Please only enter Y or N.')
 		elif command == 'shutter':
 			# should probably add wrong input check too
-			def shutterkey(string):
-				if '/' in string:
-					num1, den1 = string.split('/')
-					den1 = den1.strip()
-					num1 = num1.strip()
-					return float(num1) / float(den1)
+			resp = raw_input('Automatically sort by shutter speed? (Y/N): ')
+			if resp == 'Y':
+				auto = True
+			elif resp == 'N':
+				auto = False
+			else:
+				print('Please only enter Y or N.')
+			if not auto:
+				def shutterkey(string):
+					if '/' in string:
+						num1, den1 = string.split('/')
+						den1 = den1.strip()
+						num1 = num1.strip()
+						return float(num1) / float(den1)
+					else:
+						return float(string)
+				inp = raw_input('Enter the speed(s) in seconds separated by "," to separate into, i.e 1/200,1/20,4: ')
+				boundaries = inp.split(',')
+				boundaries = sorted([val.strip() for val in boundaries], key=shutterkey)
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					shutter(auto, True, boundaries)
+				elif copy == 'N':
+					shutter(auto, False, boundaries)
 				else:
-					return float(string)
-
-			inp = raw_input('Enter the speed(s) in seconds separated by "," to separate into, i.e 1/200,1/20,4: ')
-			boundaries = inp.split(',')
-			boundaries = sorted([val.strip() for val in boundaries], key=shutterkey)
-			copy = raw_input('Keep copy of original files? (Y/N): ')
-			if copy == 'Y':
-				x = True
-				shutter(boundaries, x)
-			elif copy == 'N':
-				x = False
-				shutter(boundaries, x)
+					print('Please only enter Y or N.')
 			else:
-				print('Please only enter Y or N.')
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					shutter(auto, True)
+				elif copy == 'N':
+					shutter(auto, False)
+				else:
+					print('Please only enter Y or N.')
 		elif command == 'iso':
-			inp = raw_input('Enter the iso(s) separated by "," to separate into, i.e 100,200,800: ')
-			boundaries = inp.split(',')
-			boundaries = sorted([val.strip() for val in boundaries])
-			copy = raw_input('Keep copy of original files? (Y/N): ')
-			if copy == 'Y':
-				x = True
-				iso(boundaries, x)
-			elif copy == 'N':
-				x = False
-				iso(boundaries, x)
+			resp = raw_input('Automatically sort by ISO? (Y/N): ')
+			if resp == 'Y':
+				auto = True
+			elif resp == 'N':
+				auto = False
 			else:
 				print('Please only enter Y or N.')
+			if not auto:
+				inp = raw_input('Enter the iso(s) separated by "," to separate into, i.e 100,200,800: ')
+				boundaries = inp.split(',')
+				boundaries = sorted([val.strip() for val in boundaries])
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					iso(auto, True, boundaries)
+				elif copy == 'N':
+					iso(auto, False, boundaries)
+				else:
+					print('Please only enter Y or N.')
+			else:
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					iso(auto, True)
+				elif copy == 'N':
+					iso(auto, False)
+				else:
+					print('Please only enter Y or N.')
 		elif command == 'aperture':
-			inp = raw_input('Enter the aperture(s) separated by "," to separate into, i.e 1.4,3.5,6,22: ')
-			boundaries = inp.split(',')
-			boundaries = sorted([val.strip() for val in boundaries], key=lambda x: float(x))
-			copy = raw_input('Keep copy of original files? (Y/N): ')
-			if copy == 'Y':
-				x = True
-				aperture(boundaries, x)
-			elif copy == 'N':
-				x = False
-				aperture(boundaries, x)
+			resp = raw_input('Automatically sort by aperture? (Y/N): ')
+			if resp == 'Y':
+				auto = True
+			elif resp == 'N':
+				auto = False
 			else:
 				print('Please only enter Y or N.')
+			if not auto:
+				inp = raw_input('Enter the aperture(s) separated by "," to separate into, i.e 1.4,3.5,6,22: ')
+				boundaries = inp.split(',')
+				boundaries = sorted([val.strip() for val in boundaries], key=lambda x: float(x))
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					aperture(auto, True, boundaries)
+				elif copy == 'N':
+					aperture(auto, False, boundaries)
+				else:
+					print('Please only enter Y or N.')
+			else:
+				copy = raw_input('Keep copy of original files? (Y/N): ')
+				if copy == 'Y':
+					aperture(auto, True)
+				elif copy == 'N':
+					aperture(auto, False)
+				else:
+					print('Please only enter Y or N.')
+		elif command == 'date':
+			date(None, True)
 		elif command == 'help':
 			print("You're outta luck bub.")
+		elif command == 'exit':
+			return
 		else:
 			print("Please input a proper command.")
 
@@ -88,11 +133,10 @@ def main():
 def import_data(path):
 	# weird input? Clarify. Right now, no home directory or beginning / needed.
 	global main_path 
-	main_path = expanduser("~") + '/' + path
+	main_path = os.path.expanduser("~") + '/' + path
 	for file_name in os.listdir(main_path):
 		try:
 			if imghdr.what(main_path + '/' + file_name):
-		# if os.path.isfile(main_path + '/' + file_name):
 				exif = check_output(['exiv2', '-p', 's', main_path + '/' + file_name])
 				one = exif.split('\n')
 				# this may not be safe. Not sure if it's always 4 at the end. CHECK.
@@ -118,7 +162,11 @@ def move_file(file_name, dest_name, copy):
 # changes the folder that the program is sorting
 def swap(path):
 	global main_path
-	main_path = expanduser("~") + '/' + path
+	global EXIF_INFO
+	main_path = os.path.expanduser("~") + '/' + path
+	EXIF_INFO = {}
+	import_data(path)
+	print('Done.')
 
 # organize by image type, maybe allow more granularity (FINE, MEDIUM, etc.)
 def type(copy):
@@ -130,59 +178,71 @@ def type(copy):
 	print("Operation complete.")
 
 # organize by shutter speed
-def shutter(boundaries, copy):
+def shutter(auto, copy, boundaries=None):
 	for image in EXIF_INFO:
 		shutter_speed = EXIF_INFO[image]["Exposure time"]
-		if '/' in shutter_speed:
-			num1, den1 = shutter_speed.split('/')
-			den1 = den1.split(' ')[0].strip()
-			num1 = num1.strip()
-			speed1 = float(num1) / float(den1)
+		if auto:
+			move_file(image, main_path + '/' + 'SHUTTER' + shutter_speed.split(' ')[0].replace('/', 'x'), copy)
 		else:
-			speed1 = float(shutter_speed.split(' ')[0].strip())
-
-		for i in range(len(boundaries)):
-			if '/' in boundaries[i]:
-				num2, den2 = boundaries[i].split('/')
-				num2 = num2.strip()
-				den2 = den2.strip()
-				speed2 = float(num2) / float(den2)
+			if '/' in shutter_speed:
+				num1, den1 = shutter_speed.split('/')
+				den1 = den1.split(' ')[0].strip()
+				num1 = num1.strip()
+				speed1 = float(num1) / float(den1)
 			else:
-				speed2 = float(boundaries[i].split('/')[0].strip())
-			if speed1 <= speed2:
-				move_file(image, main_path + '/' + 'SHUTTER' + boundaries[i].replace('/', 'x') + '-', copy)
-				break
-			if i == len(boundaries) - 1:
-				move_file(image, main_path + '/' + 'SHUTTER' + boundaries[i].replace('/', 'x') + '+', copy)
+				speed1 = float(shutter_speed.split(' ')[0].strip())
+
+			for i in range(len(boundaries)):
+				if '/' in boundaries[i]:
+					num2, den2 = boundaries[i].split('/')
+					num2 = num2.strip()
+					den2 = den2.strip()
+					speed2 = float(num2) / float(den2)
+				else:
+					speed2 = float(boundaries[i].split('/')[0].strip())
+				if speed1 <= speed2:
+					move_file(image, main_path + '/' + 'SHUTTER' + boundaries[i].replace('/', 'x') + '-', copy)
+					break
+				if i == len(boundaries) - 1:
+					move_file(image, main_path + '/' + 'SHUTTER' + boundaries[i].replace('/', 'x') + '+', copy)
 	print("Operation complete.")
 
-def iso(boundaries, copy):
+def iso(auto, copy, boundaries=None):
 	for image in EXIF_INFO:
 		iso = int(EXIF_INFO[image]["ISO speed"])
-
-		for i in range(len(boundaries)):
-			bound_iso = boundaries[i]
-			if iso <= int(bound_iso):
-				move_file(image, main_path + '/' + 'ISO' + bound_iso + '-', copy)
-				break
-			if i == len(boundaries) - 1:
-				move_file(image, main_path + '/' + 'ISO' + bound_iso + '+', copy)
+		if auto:
+			move_file(image, main_path + '/' + 'ISO' + str(iso), copy)
+		else:
+			for i in range(len(boundaries)):
+				bound_iso = boundaries[i]
+				if iso <= int(bound_iso):
+					move_file(image, main_path + '/' + 'ISO' + bound_iso + '-', copy)
+					break
+				if i == len(boundaries) - 1:
+					move_file(image, main_path + '/' + 'ISO' + bound_iso + '+', copy)
 	print("Operation complete.")
 
-def aperture(boundaries, copy):
+def aperture(auto, copy, boundaries=None):
 	for image in EXIF_INFO:
 		aperture = float(EXIF_INFO[image]["Aperture"].split('F')[1])
-
-		for i in range(len(boundaries)):
-			bound_aperture = float(boundaries[i])
-			if aperture <= bound_aperture:
-				move_file(image, main_path + '/' + 'F' + boundaries[i] + '-', copy)
-				break
-			if i == len(boundaries) - 1:
-				move_file(image, main_path + '/' + 'F' + boundaries[i] + '+', copy)
+		if auto:
+			move_file(image, main_path + '/F' + str(aperture), copy)
+		else:
+			for i in range(len(boundaries)):
+				bound_aperture = float(boundaries[i])
+				if aperture <= bound_aperture:
+					move_file(image, main_path + '/F' + boundaries[i] + '-', copy)
+					break
+				if i == len(boundaries) - 1:
+					move_file(image, main_path + '/F' + boundaries[i] + '+', copy)
 	print("Operation complete.")
 
 def date(boundaries, copy):
+	# for image in EXIF_INFO:
+	# 	datetime = time.strptime(EXIF_INFO[image]['Image timestamp'], '%Y:%m:%d %H:%M:%S')
+
+	# 	for i in range(len(boundaries)):
 	pass
+
 
 main()
